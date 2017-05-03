c704793bf347e2a86959c0f88fcea03e340698fb
Leonardo Sala <leonardo.sala@psi.ch>
2017-05-03 11:43:15 +0200

    improved performance, especially in the memory loop

--- udp_receiver.c
+++ udp_receiver.c
@@ -18,11 +18,15 @@
 #define BUFFER_LENGTH    4096
 #define YSIZE 1024
 #define XSIZE 512
+
+#define SERVER_PORT    50004
+#define SERVER_IP    "10.30.10.2"
+
 //Jungfrau
 
 // header struct for RB
 typedef struct _jungfrau_header{
-  uint16_t framenum;
+  uint64_t framenum;
   uint8_t packetnum;
   int8_t padding[64 - 2 - 1];
 } jungfrau_header;
@@ -49,7 +53,7 @@ typedef struct _jungfrau_packet{
   char framenum2[3];
   uint64_t bunchid;
   uint16_t data[BUFFER_LENGTH];
-  uint16_t framenum;
+  uint64_t framenum;
   uint8_t packetnum;
 } jungfrau_packet;
 #pragma pack(pop)
@@ -74,46 +78,56 @@ typedef struct _gotthard_packet{
 int get_message_jtb(int sd, jungfraujtb_packet * packet){
         ssize_t nbytes = recv(sd, packet, sizeof(*packet), MSG_DONTWAIT);
 
-#ifdef DEBUG
+	#ifdef DEBUG
         if(nbytes >= 0){
 	  printf("+ C %d ", getpid());
                 //printf("-%6c-\t", packet->emptyheader);
-                printf("%u ", packet->framenum);
-                printf("%u ", packet->packetnum);
-                printf("%hu ", packet->data[0]);
-                printf("%hu ", packet->data[4095]);
-                printf("%hu ", packet->data[4096]);
-                printf("%hu ", packet->data[BUFFER_LENGTH - 1]);
-                printf("%hu \n", packet->data[BUFFER_LENGTH]);
+                printf("frame %u ", packet->framenum);
+                printf("packet %u ", packet->packetnum);
+                printf("data0 %hu ", packet->data[0]);
+                printf("datalast%hu \n", packet->data[BUFFER_LENGTH - 1]);
         }
-#endif
+	#endif
         return nbytes;
 }
 
 //simple routine to get data from UDP socket
 int get_message(int sd, jungfrau_packet * packet){
-  ssize_t nbytes = recv(sd, packet, sizeof(*packet) - 2 - 1, MSG_WAITALL); //MSG_WAITALL);//MSG_DONTWAIT);
-        packet->framenum = (((int)(packet->framenum2[2])&0xff)<<16) + (((int)(packet->framenum2[1])&0xff)<<8) +((int)(packet->framenum2[0])&0xff);
+    struct sockaddr_in clientaddr;
+    int    clientaddrlen = sizeof(clientaddr);
 
-        // does not work
-        //packet->framenum =  (uint16_t)(packet->framenum2)&0x00ffffff;
-        packet->packetnum =  (uint8_t)((packet->packetnum2));
+    /* 
+       int packet_size;
+       packet_size = sizeof(*packet) - 8 - 1;
 
-	#ifdef DEBUG
-        if(nbytes >= 0){
-	  printf("+ C %d ", getpid());
-	  
-          //printf("-%6c-\t", packet->emptyheader);
-          printf("%u ", packet->framenum);
-          printf("%u ", packet->packetnum);
-          printf("%hu ", packet->data[0]);
-          printf("%hu ", packet->data[4095]);
-          printf("%hu ", packet->data[4096]);
-          printf("%hu ", packet->data[BUFFER_LENGTH - 1]);
-          printf("%hu \n", packet->data[BUFFER_LENGTH]);
-        }
-	#endif
-        return nbytes;
+     */
+
+    //ssize_t nbytes = recvfrom(sd, packet, sizeof(*packet) - 8 - 1, 0, (struct sockaddr *)&clientaddr, &clientaddrlen);
+    ssize_t nbytes = recvfrom(sd, packet, sizeof(*packet), 0, 
+			      (struct sockaddr *)&clientaddr, &clientaddrlen);
+  
+
+//ssize_t nbytes = recv(sd, packet, sizeof(*packet) - 8 - 1, 0); //, MSG_DONTWAIT);
+    packet->framenum = (((int)(packet->framenum2[2])&0xff)<<16) + (((int)(packet->framenum2[1])&0xff)<<8) +((int)(packet->framenum2[0])&0xff);
+  
+    packet->packetnum =  (uint16_t)((packet->packetnum2));
+
+    
+  #ifdef DEBUG
+  if(nbytes >= 0){
+    printf("+ C %d ", getpid());
+    
+    //printf("-%6c-\t", packet->emptyheader);
+    printf("+ C %d ", getpid());
+    //printf("-%6c-\t", packet->emptyheader);
+    printf("frame %u ", packet->framenum);
+    printf("packet %u ", packet->packetnum);
+    printf("data0 %hu ", packet->data[0]);
+    printf("datalast %hu \n", packet->data[BUFFER_LENGTH - 1]);
+  }
+  #endif
+    
+  return nbytes;
 }
 
 
@@ -166,10 +180,11 @@ int put_data_in_memory(int sd, jungfrau_packet * packet, int n_entries, uint16_t
       //}
     }
   }
+  /*
   else{
     packet->framenum = 65535;
   }
-
+  */
   return data_len;
 }
 
@@ -245,14 +260,15 @@ int put_udp_in_rb(int sock, int bit_depth, int rb_current_slot, int rb_header_id
 int put_data_in_rb(int sock, int bit_depth, int rb_current_slot, int rb_header_id, int rb_hbuffer_id, int rb_dbuffer_id, int rb_writer_id, int32_t * idx, int16_t nframes){
   
   int n_recv_frames = 0;
-  uint16_t framenum_last = 0;
-  uint16_t framenum = 0;
+  uint64_t framenum_last = 0;
+  uint64_t framenum = 0;
   int total_packets = 0;
+  int stat_total_frames = 0;
   int lost_frames = 0;
   int tot_lost_frames = 0;
   int64_t lost_packets = 0;
   int64_t tot_lost_packets = 0;
-  time_t ti = 0;
+  //time_t ti = 0;
 
   int temp = 0;
   
@@ -268,127 +284,192 @@ int put_data_in_rb(int sock, int bit_depth, int rb_current_slot, int rb_header_i
   int packets_frame;
   int *ret;
   //end
-  
-  
+  clock_t ti2;  
+
+  int packets_frame_recv[128];
+
+  struct  timeval ti, te; //for timing
+  double tdif=-1;
+
   rb_set_buffer_stride_in_byte(rb_dbuffer_id, 2 * 512 * 3 * 1024);
   rb_adjust_nslots(rb_header_id);
   
+  printf("| PID | cur_frame | Hz | Lost packets | \% lost packets |\n");
   
+  for(i=0; i< 128; i++)
+    packets_frame_recv[i] = 0;
+
+  // claim a slot before starting
+  rb_current_slot = rb_claim_next_slot(rb_writer_id);
+
   while(true){
+    //printf("A\n");
     if(nframes != -1)
       if(n_recv_frames >= nframes)
 	break;
-    
-    //rb_current_slot = put_udp_in_rb(sock, bit_depth, rb_current_slot, rb_header_id, rb_hbuffer_id, rb_dbuffer_id, rb_writer_id, idx, &framenum);
-    
-    
-    //begin
-    
-    //printf("PID %d ID %d\n", getpid(), rb_writer_id);
-    
+        
     n_entries = BUFFER_LENGTH; // / (bit_depth / 8);
     //data_len = put_data_in_memory(sock, &packetb, n_entries, p1, ph, idx);
     
     data_len = get_message(sock, &packet);
     packets_frame = 127;
-    
-    if(data_len > 0){
 
+    if(data_len > 0){
+      
       if(framenum == 0)
 	framenum = packet.framenum;
+
+      if(framenum_last == 0)
+	framenum_last = framenum;
       
-      //printf("%d %d packet num %d\n", getpid(), packet.framenum, packet.packetnum);
-	//	printf("PID %d ID %d\n", getpid(), rb_writer_id);
-      //	printf("PID %d %d %d\n", getpid(), rb_header_id, rb_hbuffer_id);
+      packets_frame_recv[packet.packetnum] = packet.packetnum + 1;
       
-      if(packet.packetnum == 127 || packet.framenum != framenum){
-	//this means a new frame
+      /*
+      printf("%d %d packet num %d\n", getpid(), packet.framenum, packet.packetnum);
+      	printf("PID %d ID %d\n", getpid(), rb_writer_id);
+      	printf("PID %d %d %d\n", getpid(), rb_header_id, rb_hbuffer_id);
+      */
+
+      //this means a new frame
+      if(framenum != framenum_last ){
 	if(rb_current_slot != -1)
 	  rb_commit_slot(rb_writer_id, rb_current_slot);
 	rb_current_slot = rb_claim_next_slot(rb_writer_id);
-	while(rb_current_slot == -1)
-	  rb_current_slot = rb_claim_next_slot(rb_writer_id);
-      }
+	
+	if(rb_current_slot == -1)
+	  while(rb_current_slot == -1)
+	    rb_current_slot = rb_claim_next_slot(rb_writer_id);
+
+	// still gives me wrong number it seems, +1
+	if(total_packets != 128){
+	  //printf("total_packets %d\n", total_packets);
+	  lost_frames ++;
+	  tot_lost_frames += 1;
+	  lost_packets += 128 - total_packets;
+	  tot_lost_packets += 128 - total_packets;
+	  
+	  /*
+	  for(j=0; j< 128; j++)
+	    printf("%d ", packets_frame_recv[j]);
+	  printf("\n");
+	  */
+	  
+	}
+	for(i=0; i< 128; i++)
+	  packets_frame_recv[i] = 0;
+	
+	framenum_last = framenum;
+	stat_total_frames ++;
+	n_recv_frames ++;
+	
+	if (n_recv_frames % 5000 == 0){
+	  gettimeofday(&te, NULL);
+	  tdif = (1e6 * (te.tv_sec - ti.tv_sec) + (long)(te.tv_usec) - (long)(ti.tv_usec)) / 1e6;
+	  //printf("%f %d %d", tdif, te.tv_sec, ti.tv_sec);
+	  //printf("| PID | cur_frame | Hz | Lost packets | %lost packets |\n");
+	  printf("| %d | %d | %.2f | %d | %.1f |\n",
+		 getpid(), framenum_last, (double) 5000. / tdif, lost_packets, 
+		 100. * (float)lost_packets / (float)(128 * stat_total_frames)
+		 );
+	  //ti = time(NULL);
+	  gettimeofday(&ti,NULL);
+	  //ti2 = clock();
+	  lost_frames = 0;
+	  lost_packets = 0;
+	  stat_total_frames = 0;
+	}
+	total_packets = 0;
+      } // end new frame if
     
       ph = (jungfrau_header *) rb_get_buffer_slot(rb_hbuffer_id, rb_current_slot);
       p1 = (uint16_t *) rb_get_buffer_slot(rb_dbuffer_id, rb_current_slot);
     
-      for(i=0; i < (sizeof(packet.data) / sizeof(uint16_t)); i++){
-      	p1[idx[i + n_entries * (packets_frame - packet.packetnum)]] = packet.data[i];
+      int shift = n_entries * (packets_frame - packet.packetnum);
+      for(i=0; i < BUFFER_LENGTH; i++){
+      	p1[idx[i + shift]] = packet.data[i];
       }
-    
+
       header.framenum = packet.framenum;
       framenum = header.framenum;
       
       if(ph->framenum != packet.framenum){
 	memcpy(ph, &header, sizeof(header));
       }
-    }
-    else
-      continue;
-    
-    if(rb_current_slot == -1)
-      continue;
-
-    if(ti == 0)
-      ti = time(NULL);
-    
-    if(framenum_last == 0)
-      framenum_last = framenum;
-
-    if(framenum != framenum_last ){
-      if(total_packets != 128){
-	lost_frames ++;
-	tot_lost_frames += 1;
-	lost_packets += 128 - total_packets;
-	tot_lost_packets += 128 - total_packets;
-      }
-
-      framenum_last = framenum;
-      total_packets = 0;
-      n_recv_frames ++;
-
-      if (n_recv_frames % 1000 == 0){
-      	printf("Computed frame rate at frame %d: %.2f Hz Lost packets / frames: %d / %d (%d / %d)\n",
-      	       framenum, 1000. / (float)(time(NULL) - ti), lost_packets, lost_frames, tot_lost_packets, tot_lost_frames);
-	ti = time(NULL);
-	lost_frames = 0;
-	lost_packets = 0;
-      }
-    }
-    total_packets ++;
-
+     
+      // This should cast an error, or a warning
+      if(rb_current_slot == -1)
+	continue;
+      
+      if(tdif < 0){
+	//ti2 = clock();
+	tdif = 0;
+	gettimeofday(&ti, NULL);
+      }    
+      
+      total_packets ++;
+    } // end data if
   }
 
   return 0;
 }
 
 /*
-int main(){
-	int ret;
+int main(int argc, char *argv[]){
+  int ret, sd, rc;
+  int data_len;
+  struct sockaddr_in serveraddr;	
+  jungfrau_packet packet;
+  int data[1000];
+  int recv_packets;
+  int recv_frames;
+  int last_frame;
+  int lost_packets;
 
-	sd = socket(AF_INET, SOCK_DGRAM, 0);
-	printf("SD: %d\n", sd);
+  sd = socket(AF_INET, SOCK_DGRAM, 0);
+  printf("SD: %d\n", sd);
+  
+  int val=1000 * 1024 * 1024;
+  setsockopt(sd, SOL_SOCKET, SO_RCVBUF, &val, sizeof(int));
+  
+  if (sd < 0){
+    perror("socket() failed");
+    return -1;
+  }
+  
+  memset(&serveraddr, 0, sizeof(serveraddr));
+  serveraddr.sin_family      = AF_INET;
+  serveraddr.sin_port        = htons(atoi(argv[2]));
+  serveraddr.sin_addr.s_addr = inet_addr(argv[1]);
+  
+  rc = bind(sd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+  printf("RC bind: %d %s %d \n", rc, "10.30.10.2", atoi(argv[2]));
+  
+  recv_packets = 0;
+  recv_frames = 0;
+  last_frame = -1;
+  printf("PID Frames Lost Perc\n");
+  while(1==1){
+    data_len = get_message(sd, &packet);
 
-	int   val=SOCKET_BUFFER_SIZE;
-	setsockopt(sd, SOL_SOCKET, SO_RCVBUF, &val, sizeof(int));
-	
-	if (sd < 0){
-	perror("socket() failed");
-	return -1;
-	}
-	
-	memset(&serveraddr, 0, sizeof(serveraddr));
-	serveraddr.sin_family      = AF_INET;
-	serveraddr.sin_port        = htons(SERVER_PORT);
-	serveraddr.sin_addr.s_addr = inet_addr(SERVER_IP);
-	
-	rc = bind(sd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
-	printf("RC bind: %d\n", rc);
-	//ret = setsockopt(socket, SOL_SOCKET, SO_RCVBUF, &val, sizeof(int));
-	//printf("%d\n", ret);
+    if (data_len == 0)
+      continue;
+    
+    if (last_frame == -1)
+      last_frame = packet.framenum;
 
-	while(1)
-		get_message(sd);
+    if (last_frame != packet.framenum){
+      recv_frames ++;
+      last_frame = packet.framenum;
+    }
+
+    if (recv_frames % 1000 == 0 && packet.packetnum == 127 && recv_frames != 0){
+      lost_packets = 128 * recv_frames - recv_packets;
+      printf("%d %d %d %.1f\n", getpid(), recv_frames, lost_packets, 100. * (float)(lost_packets) / (float)(128 * recv_frames));
+    }
+    recv_packets ++;
+
+  }
+  return 0;
 }
+
 */
