72f24aa93c4f93cf27982af9ad18c9e97cb4dde7
babic_a <andrej.babic@psi.ch>
2019-01-28 15:22:45 +0100

    Add extern declaration for inline functions

--- tests/debug/debug_single_module.c
+++ tests/debug/debug_single_module.c
@@ -9,6 +9,43 @@ char const RB_HEADER_FILENAME[] = "/dev/shm/rb/rb_header.dat";
 char const RB_HBUFFER_FILENAME[] = "/dev/shm/rb/rb_image_header.dat";
 char const RB_DBUFFER_FILENAME[] = "/dev/shm/rb/rb_image_data.dat";
 
+
+// udp_receiver.c
+extern inline bool receive_packet (int sock, char* udp_packet, size_t udp_packet_bytes, 
+  barebone_packet* bpacket, detector_definition* det_definition );
+extern inline void save_packet (
+  barebone_packet* bpacket, rb_metadata* rb_meta, 
+  counter* counters, detector* det, detector_definition* det_definition);
+extern int put_data_in_rb(
+  int sock, int bit_depth, int rb_current_slot, int rb_header_id, int rb_hbuffer_id, int rb_dbuffer_id, int rb_writer_id, 
+  int16_t n_frames, float timeout, detector det);
+
+// utils_metadata.c
+extern inline uint32_t get_current_module_offset_in_pixels (detector det);
+extern int get_current_module_index (detector det);
+extern int get_n_lines_per_packet (detector det, size_t data_bytes_per_packet, int bit_depth);
+extern inline int get_n_packets_per_frame (detector det, size_t data_bytes_per_packet, int bit_depth);
+extern inline rb_metadata get_ringbuffer_metadata (
+  int rb_writer_id, int rb_header_id, int rb_hbuffer_id, int rb_dbuffer_id, int rb_current_slot,  
+    detector det, size_t data_bytes_per_packet, int bit_depth );
+
+// utils_receiver.c
+extern inline bool is_timeout_expired (double timeout, struct timeval timeout_start_time);
+extern inline int get_udp_packet (int socket_fd, char* buffer, size_t buffer_len);
+extern inline bool is_slot_ready_for_frame (uint64_t frame_number, counter *counters);
+extern inline bool is_frame_complete (int n_packets_per_frame, counter* counters);
+extern inline void print_statistics (counter* counters, struct timeval* last_stats_print_time);
+extern inline int get_packet_line_number(rb_metadata* rb_meta, uint32_t packet_number);
+extern inline bool is_acquisition_completed(int16_t n_frames, counter* counters);
+extern inline void initialize_counters_for_new_frame (counter* counters, uint64_t frame_number);
+
+// utils_ringbuffer.c
+extern inline bool commit_slot (int rb_writer_id, int rb_current_slot);
+extern inline void commit_if_slot_dangling (counter* counters, rb_metadata* rb_meta);
+extern inline void claim_next_slot(rb_metadata* rb_meta);
+extern inline void initialize_rb_header (rb_metadata* rb_meta);
+extern inline void update_rb_header (rb_metadata* rb_meta, barebone_packet* bpacket, counter *counters);
+
 int setup_udp_socket(int udp_port, int rcv_buffer)
 {
     int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -26,7 +63,7 @@ int setup_udp_socket(int udp_port, int rcv_buffer)
 
     setsockopt(socket_fd, SOL_SOCKET, SO_RCVBUF, &rcv_buffer, sizeof(int));
 
-    if (bind(socket_fd, (struct socketaddr*)&server, size_of(server)))
+    if (bind(socket_fd, &server, sizeof(server)))
     {
         printf("Cannot bind to socket.\n");
         exit(1);
@@ -46,7 +83,7 @@ int setup_rb(int* rb_header_id, int* rb_writer_id, int* rb_hbuffer_id, int* rb_d
     rb_set_buffer_stride_in_byte(*rb_hbuffer_id, FRAME_HEADER_BYTES);
 
     *rb_dbuffer_id = rb_attach_buffer_to_header(RB_DBUFFER_FILENAME, *rb_header_id, 0);
-    rb_set_buffer_stride_in_byte(*rb_dbuffer_id, FRAME_DATA_BYTES)
+    rb_set_buffer_stride_in_byte(*rb_dbuffer_id, FRAME_DATA_BYTES);
 
     return rb_adjust_nslots(*rb_header_id);
 }
@@ -54,7 +91,7 @@ int setup_rb(int* rb_header_id, int* rb_writer_id, int* rb_hbuffer_id, int* rb_d
 detector get_eiger9M_definition()
 {
     detector det;
-    strlcpy(det.detector_name, "EIGER", 5);
+    //strlcpy(det.detector_name, "EIGER", 5);
     det.submodule_n = 4;
 
     det.detector_size[0] = 3264;
@@ -94,14 +131,14 @@ int main(int argc, char *argv[])
     }
 
     int udp_port = atoi(argv[1]);
-    int udp_rcv_buffer = 10000 * 1024 * 1024
+    int udp_rcv_buffer = 1000 * 1024 * 1024;
     int socket_fd = setup_udp_socket(udp_port, udp_rcv_buffer);
 
     int bit_depth = atoi(argv[2]);
 
     int rb_current_slot = -1;
     int rb_header_id, rb_writer_id, rb_hbuffer_id, rb_dbuffer_id;
-    int n_slots = setup_rb(&rb_header_id, &rb_writer_id, &rb_hbuffer_id, &rb_dbuffer_id);
+    setup_rb(&rb_header_id, &rb_writer_id, &rb_hbuffer_id, &rb_dbuffer_id);
 
     int n_frames = 2;
     int timeout = 100000;
