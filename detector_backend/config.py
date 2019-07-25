ba55c24c57a79e8cd0d491397c838c921ef91f0f
Andrej Babic <andrej.babic@psi.ch>
2019-07-25 17:34:14 +0200

    Add config file to project

---
+++ detector_backend/config.py
@@ -0,0 +1,7 @@
+CONFIG_SENDER_RANK = 0
+
+DEFAULT_RB_FOLDER = "/dev/shm/rb/"
+DEFAULT_RB_HEAD_FILE = "rb_header.dat"
+DEFAULT_RB_IMAGE_HEAD_FILE = "rb_image_header.dat"
+DEFAULT_RB_IMAGE_DATA_FILE = "rb_image_data.dat"
+
