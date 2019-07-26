112721190b1cc5137be6b01c2a883e490ba44bef
Andrej Babic <andrej.babic@psi.ch>
2019-07-26 08:51:57 +0200

    Consolidate RB logic in one file

--- detector_backend/utils_ringbuffer.py
+++ detector_backend/utils_ringbuffer.py
@@ -1,3 +1,4 @@
+import ctypes
 import os
 from logging import getLogger
 
@@ -15,6 +16,7 @@ class RingBuffer(object):
     def __init__(self,
                  process_id,
                  follower_ids,
+                 detector,
                  rb_folder=config.DEFAULT_RB_FOLDER,
                  rb_head_file=config.DEFAULT_RB_HEAD_FILE,
                  rb_image_head_file=config.DEFAULT_RB_IMAGE_HEAD_FILE,
@@ -22,6 +24,8 @@ class RingBuffer(object):
 
         self.process_id = process_id
         self.follower_ids = follower_ids
+        self.detector = detector
+
         self.rb_folder = rb_folder
         self.rb_head_file = rb_head_file
         self.rb_image_head = rb_image_head_file
@@ -31,6 +35,9 @@ class RingBuffer(object):
         self.rb_reader_id = None
         self.rb_hbuffer_id = None
         self.rb_dbuffer_id = None
+
+        self.image_header_n_bytes = None
+        self.image_data_n_bytes = None
 
     def init_buffer(self, setup_header_file=False):
 
@@ -56,15 +63,15 @@ class RingBuffer(object):
         self.rb_hbuffer_id = rb.attach_buffer_to_header(self.rb_image_head, self.rb_header_id, 0)
         self.rb_dbuffer_id = rb.attach_buffer_to_header(self.rb_image_data, self.rb_header_id, 0)
 
-        # 64 bytes = 8 * uint64_t == 8 * (64bit/8bit)
-        frame_header_n_bytes = 64 * self.n_submodules
-        rb.set_buffer_stride_in_byte(self.rb_hbuffer_id, frame_header_n_bytes)
+        self.image_header_n_bytes = self.detector.n_submodules_total * config.IMAGE_HEADER_SUBMODULE_SIZE_BYTES
+        rb.set_buffer_stride_in_byte(self.rb_hbuffer_id, self.image_header_n_bytes)
 
-        frame_data_n_bytes = int((self.detector_size[0] * self.detector_size[1] * self.bit_depth) / 8)
-        rb.set_buffer_stride_in_byte(self.rb_dbuffer_id, frame_data_n_bytes)
+        self.image_data_n_bytes = (self.detector.detector_size[0] *
+                                   self.detector.detector_size[1] * self.detector.bit_depth) / 8
+        rb.set_buffer_stride_in_byte(self.rb_dbuffer_id, self.image_data_n_bytes)
 
         n_slots = rb.adjust_nslots(self.rb_header_id)
-        rb.set_buffer_slot_dtype(dtype=ctypes.__getattribute__('c_uint' + str(self.bit_depth)))
+        rb.set_buffer_slot_dtype(dtype=ctypes.__getattribute__('c_uint' + str(self.detector.bit_depth)))
 
         _logger.info("RB %d slots: %d" % (self.rb_header_id, n_slots))
         _logger.info("RB header stride: %d" % rb.get_buffer_stride_in_byte(self.rb_hbuffer_id))
