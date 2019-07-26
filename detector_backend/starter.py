3e6b0e255e3da5e850e1fe6e7fa25530d0a37b6f
Andrej Babic <andrej.babic@psi.ch>
2019-07-26 16:34:21 +0200

    Extract standard starter script

--- detector_backend/starter.py
+++ detector_backend/starter.py
@@ -1,76 +1,72 @@
 from mpi4py import MPI
 
 from detector_backend.module.rest import start_rest_api
-from detector_backend.utils_detector import DetectorDefinition, EIGER
 from detector_backend.get_ipports_fromcfg import get_ips_ports_fromcfg
 from detector_backend.module.udp_receiver import start_udp_receiver
 from detector_backend.module.zmq_sender import start_writer_sender
-
 from detector_backend.utils_ringbuffer import RingBuffer
 
-eiger9m = DetectorDefinition(
-    detector_name="cSAXS Eiger 9m",
-    detector_model=EIGER,
-    geometry=[6, 3],
-    bit_depth=32
-)
 
-udp_ips, udp_ports = get_ips_ports_fromcfg("tmp.config")
+def start_standard_setup(detector_definition, detector_config_filename):
 
-RECEIVER_RANKS = eiger9m.get_receiver_ranks()
-SENDER_RANK = RECEIVER_RANKS[-1] + 1
-PREVIEW_RANK = RECEIVER_RANKS[-1] + 2
+    current_process_rank = MPI.COMM_WORLD.rank
+    total_processes = MPI.COMM_WORLD.size
 
-current_process_rank = MPI.COMM_WORLD.rank
-total_processes = MPI.COMM_WORLD.size
+    udp_ips, udp_ports = get_ips_ports_fromcfg(detector_config_filename)
 
-# total processes = total receivers + rest api + sender + preview
-total_expected_processes = len(RECEIVER_RANKS) + 3
+    # All UDP receiving ranks, writer sender, preview sender, REST Api.
+    receiver_ranks = list(range(detector_definition.n_submodules_total))
+    sender_rank = receiver_ranks[-1] + 1
+    preview_rank = sender_rank + 1
+    rest_rank = preview_rank + 1
 
-if total_processes != total_expected_processes:
-    raise ValueError("Expected %d total processes, but got %d. Fix mpi-run procedure.")
+    # total processes = total receivers + sender + preview + rest api
+    total_expected_processes = len(receiver_ranks) + 3
 
-# The last rank is always the REST api.
-if current_process_rank == total_processes - 1:
-    start_rest_api(host="0.0.0.0", port=8080)
+    if total_processes != total_expected_processes:
+        raise ValueError("Expected %d total processes, but got %d. Fix mpi-run call.")
 
-elif current_process_rank in RECEIVER_RANKS:
+    # The last rank is always the REST api.
+    if current_process_rank == rest_rank:
+        start_rest_api(host="0.0.0.0", port=8080, ringbuffer=None)
 
-    start_udp_receiver(udp_ip=udp_ips[current_process_rank],
-                       udp_port=udp_ports[current_process_rank],
-                       detector_def=eiger9m,
-                       ringbuffer=RingBuffer(
-                           process_id=current_process_rank,
-                           follower_ids=[SENDER_RANK, PREVIEW_RANK],
-                           detector_config=eiger9m
-                       ),
-                       module_id=current_process_rank // 4,
-                       submodule_id=current_process_rank % 4
-                       )
+    elif current_process_rank in receiver_ranks:
 
-elif current_process_rank == SENDER_RANK:
+        start_udp_receiver(udp_ip=udp_ips[current_process_rank],
+                           udp_port=udp_ports[current_process_rank],
+                           detector_def=detector_definition,
+                           ringbuffer=RingBuffer(
+                               process_id=current_process_rank,
+                               follower_ids=[sender_rank, preview_rank],
+                               detector_config=detector_definition
+                           ),
+                           module_id=current_process_rank // 4,
+                           submodule_id=current_process_rank % 4
+                           )
 
-    start_writer_sender(name="Writer Sender",
-                        bind_url="tcp://localhost:40000",
-                        zmq_mode="PUSH",
-                        detector_def=eiger9m,
-                        ringbuffer=RingBuffer(
-                            process_id=current_process_rank,
-                            follower_ids=RECEIVER_RANKS,
-                            detector_config=eiger9m
-                        ))
+    elif current_process_rank == sender_rank:
 
-elif current_process_rank == PREVIEW_RANK:
-    pass
-    # start_preview_sender(name="Preview Sender",
-    #                      bind_url="tcp://localhost:50000",
-    #                      zmq_mode="PUB",
-    #                      detector_def=eiger9m,
-    #                      ringbuffer=RingBuffer(
-    #                          process_id=current_process_rank,
-    #                          follower_ids=RECEIVER_RANKS,
-    #                          detector_config=eiger9m
-    #                      ))
+        start_writer_sender(name="Writer Sender",
+                            bind_url="tcp://localhost:40000",
+                            zmq_mode="PUSH",
+                            detector_def=detector_definition,
+                            ringbuffer=RingBuffer(
+                                process_id=current_process_rank,
+                                follower_ids=receiver_ranks,
+                                detector_config=detector_definition
+                            ))
 
-else:
-    raise ValueError("Process with rank %d is not assigned to any module." % current_process_rank)
+    elif current_process_rank == preview_rank:
+        pass
+        # start_preview_sender(name="Preview Sender",
+        #                      bind_url="tcp://localhost:50000",
+        #                      zmq_mode="PUB",
+        #                      detector_def=detector_definition,
+        #                      ringbuffer=RingBuffer(
+        #                          process_id=current_process_rank,
+        #                          follower_ids=RECEIVER_RANKS,
+        #                          detector_config=detector_definition
+        #                      ))
+
+    else:
+        raise ValueError("Process with rank %d is not assigned to any module." % current_process_rank)
