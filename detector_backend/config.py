9856a90c76fa41b400d8243d9ca85e655bd8d8cf
Andrej Babic <andrej.babic@psi.ch>
2019-07-26 09:40:19 +0200

    Add header bytes size

--- detector_backend/config.py
+++ detector_backend/config.py
@@ -3,3 +3,5 @@ DEFAULT_RB_HEAD_FILE = "rb_header.dat"
 DEFAULT_RB_IMAGE_HEAD_FILE = "rb_image_header.dat"
 DEFAULT_RB_IMAGE_DATA_FILE = "rb_image_data.dat"
 
+# 64 bytes per submodule = 8 * uint64_t values == 8 * (64bit/8bit)
+IMAGE_HEADER_SUBMODULE_SIZE_BYTES = 64
