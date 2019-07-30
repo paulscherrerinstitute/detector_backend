3c2a22cebb65341a1e5ef678bab07993e9cb4111
Andrej Babic <andrej.babic@psi.ch>
2019-07-30 17:23:16 +0200

    Add ringbuffer Master to rest api

--- detector_backend/starter.py
+++ detector_backend/starter.py
@@ -4,7 +4,7 @@ from detector_backend.module.rest import start_rest_api
 from detector_backend.get_ipports_fromcfg import get_ips_ports_fromcfg
 from detector_backend.module.udp_receiver import start_udp_receiver
 from detector_backend.module.zmq_sender import start_writer_sender, start_preview_sender
-from detector_backend.utils_ringbuffer import RingBuffer
+from detector_backend.mpi_ringbuffer import MpiRingBufferMaster, MpiRingBufferClient
 
 
 def start_standard_setup(detector_definition, detector_config_filename):
@@ -28,14 +28,14 @@ def start_standard_setup(detector_definition, detector_config_filename):
 
     # The last rank is always the REST api.
     if current_process_rank == rest_rank:
-        start_rest_api(host="0.0.0.0", port=8080, ringbuffer=None)
+        start_rest_api(host="0.0.0.0", port=8080, ringbuffer=MpiRingBufferMaster())
 
     elif current_process_rank in receiver_ranks:
 
         start_udp_receiver(udp_ip=udp_ips[current_process_rank],
                            udp_port=udp_ports[current_process_rank],
                            detector_def=detector_definition,
-                           ringbuffer=RingBuffer(
+                           ringbuffer=MpiRingBufferClient(
                                process_id=current_process_rank,
                                follower_ids=[sender_rank, preview_rank],
                                detector_config=detector_definition
@@ -50,7 +50,7 @@ def start_standard_setup(detector_definition, detector_config_filename):
                             bind_url="tcp://localhost:40000",
                             zmq_mode="PUSH",
                             detector_def=detector_definition,
-                            ringbuffer=RingBuffer(
+                            ringbuffer=MpiRingBufferClient(
                                 process_id=current_process_rank,
                                 follower_ids=receiver_ranks,
                                 detector_config=detector_definition
@@ -62,7 +62,7 @@ def start_standard_setup(detector_definition, detector_config_filename):
                              bind_url="tcp://localhost:50000",
                              zmq_mode="PUB",
                              detector_def=detector_definition,
-                             ringbuffer=RingBuffer(
+                             ringbuffer=MpiRingBufferClient(
                                  process_id=current_process_rank,
                                  follower_ids=receiver_ranks,
                                  detector_config=detector_definition
