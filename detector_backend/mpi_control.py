a42791c661cf4d1162877fb0aa84bdc78d56ed51
Andrej Babic <andrej.babic@psi.ch>
2019-08-07 17:21:04 +0200

    Improve MpiControlMaster with logging

--- detector_backend/mpi_control.py
+++ detector_backend/mpi_control.py
@@ -1,4 +1,9 @@
+import logging
+
 from mpi4py import MPI
+
+_logger_master = logging.getLogger("mpi_control_master")
+_logger_client = logging.getLogger("mpi_control_client")
 
 
 class MpiControlClient(object):
@@ -26,10 +31,27 @@ class MpiControlMaster(object):
     def __init__(self):
         self.communicator = MPI.COMM_WORLD
         self.master_process_rank = self.communicator.size-1
+        # Master process rank is the last rank. All other all clients.
+        self.client_ranks = list(range(self.master_process_rank))
+
+        _logger_master.info("Starting control master with master_process_rank=%d and client_ranks=%s" %
+                            (self.master_process_rank, self.client_ranks))
 
         if self.communicator.rank != self.master_process_rank:
             raise ValueError("The config master must be instantiated on the "
                              "master process with rank %d." % self.master_process_rank)
 
     def send_message(self, message):
-        self.communicator.bcast(message, root=self.master_process_rank)
+
+        _logger_master.info("Sending %s to all clients." % message)
+
+        requests = []
+
+        for client_rank in self.client_ranks:
+            requests.append(self.communicator.issend(message, dest=client_rank))
+
+        _logger_master.debug("Waiting for clients to receive message.")
+
+        MPI.Request.Waitall(requests)
+
+        _logger_master.info("All clients received message.")
