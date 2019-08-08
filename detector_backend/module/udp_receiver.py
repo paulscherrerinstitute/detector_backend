1f0de1a3efdee1bbb6b1cb9a180cb017d044df5c
Andrej Babic <andrej.babic@psi.ch>
2019-08-08 14:01:47 +0200

    Change UDP receive function signature
    We do not calculate the detector configuration in C code,
    but in Python and pass it to the function in a struct form.

--- detector_backend/module/udp_receiver.py
+++ detector_backend/module/udp_receiver.py
@@ -12,6 +12,8 @@ from copy import copy
 
 from detector_backend import config
 from detector_backend.mpi_control import MpiControlClient
+from detector_backend.utils_detectors import get_n_bytes_per_submodule_line, get_n_bytes_per_frame_line, \
+    get_n_packets_per_frame, get_n_lines_per_packet, get_module_coordinates, get_submodule_coordinates
 
 _logger = getLogger("udp_receiver")
 
@@ -34,6 +36,25 @@ class CDetDef(ctypes.Structure):
     gap_px_modules = [0, 0]
 
 
+class CRbMetadata(ctypes.Structure):
+    _fields_ = [
+        ('rb_writer_id', ctypes.c_uint),
+        ('rb_header_id', ctypes.c_uint),
+
+        ('rb_hbuffer_id', ctypes.c_uint),
+        ('rb_dbuffer_id', ctypes.c_uint),
+
+        ('mod_origin', ctypes.c_uint32),
+        ('mod_number', ctypes.c_uint),
+        ('n_lines_per_packet', ctypes.c_uint),
+        ('n_packets_per_frame', ctypes.c_uint),
+        ('bit_depth', ctypes.c_uint),
+
+        ('n_bytes_per_frame_line', ctypes.c_uint32),
+        ('n_bytes_per_submodule_line', ctypes.c_uint32),
+    ]
+
+
 def get_udp_receive_function():
     expected_library_location = os.path.dirname(os.path.realpath(__file__)) + "/../../libudpreceiver.so"
 
@@ -41,11 +62,7 @@ def get_udp_receive_function():
         _mod = ctypes.cdll.LoadLibrary(expected_library_location)
 
         put_data_in_rb = _mod.put_data_in_rb
-
-        put_data_in_rb.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_int,
-                                   ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int,
-                                   ctypes.c_uint32, ctypes.c_float, CDetDef)
-
+        put_data_in_rb.argtypes = (ctypes.c_int, CRbMetadata, CDetDef, ctypes.c_uint32, ctypes.c_float)
         put_data_in_rb.restype = ctypes.c_int
 
         return put_data_in_rb
@@ -69,20 +86,32 @@ def get_c_det_def(detector_def, module_id, submodule_id):
     c_det_def.gap_px_chips = copy(as_ctypes(array(detector_model.gap_px_chips, dtype="uint16", order="C")))
     c_det_def.gap_px_modules = copy(as_ctypes(array(detector_model.gap_px_modules, dtype="uint16", order="C")))
 
-    # FIXME: change to row and columnwise option
-    # Column-first numeration
-    if detector_model == "EIGER":
-        mod_indexes = array([module_id % detector_def.geometry[0],
-                             int(module_id / detector_def.geometry[0])], dtype="int32", order='C')
-    # Row-first numeration
-    else:
-        mod_indexes = array([int(module_id / detector_def.geometry[1]),
-                             module_id % detector_def.geometry[1]], dtype="int32", order='C')
-
-    c_det_def.module_idx = as_ctypes(mod_indexes)
-    c_det_def.submodule_idx = as_ctypes(array([int(submodule_id / 2), submodule_id % 2], dtype="int32", order='C'))
+    c_det_def.module_idx = as_ctypes(get_module_coordinates(detector_def, module_id))
+    c_det_def.submodule_idx = as_ctypes(array(get_submodule_coordinates(submodule_id), dtype="int32", order='C'))
 
     return c_det_def
+
+
+def get_c_rb_metadata(detector_def, ringbuffer):
+    c_rb_meta = CRbMetadata()
+
+    c_rb_meta.rb_writer_id = ringbuffer.process_id
+    c_rb_meta.rb_header_id = ringbuffer.rb_header_id
+
+    c_rb_meta.rb_hbuffer_id = ringbuffer.rb_hbuffer_id
+    c_rb_meta.rb_dbuffer_id = ringbuffer.rb_dbuffer_id
+
+    c_rb_meta.mod_origin = get_current_module_offset_in_pixels(detector_def)
+    c_rb_meta.mod_number = get_current_module_index(detector_def)
+
+    c_rb_meta.n_lines_per_packet = get_n_lines_per_packet(detector_def)
+    c_rb_meta.n_packets_per_frame = get_n_packets_per_frame(detector_def)
+    c_rb_meta.bit_depth = detector_def.bit_depth
+
+    c_rb_meta.n_bytes_per_frame_line = get_n_bytes_per_frame_line(detector_def)
+    c_rb_meta.n_bytes_per_submodule_line = get_n_bytes_per_submodule_line(detector_def)
+
+    return c_rb_meta
 
 
 def start_udp_receiver(udp_ip, udp_port, detector_def, ringbuffer, module_id, submodule_id):
@@ -99,6 +128,7 @@ def start_udp_receiver(udp_ip, udp_port, detector_def, ringbuffer, module_id, su
     udp_socket.bind((udp_ip, udp_port))
 
     c_det_def = get_c_det_def(detector_def, module_id, submodule_id)
+    c_rb_meta = get_c_rb_metadata(detector_def, ringbuffer)
 
     # Function signature
     # int sock, int bit_depth,
@@ -112,11 +142,7 @@ def start_udp_receiver(udp_ip, udp_port, detector_def, ringbuffer, module_id, su
 
         _logger.debug("[%d] Starting udp_receive function." % udp_port)
 
-        udp_receive(
-            udp_socket.fileno(), detector_def.bit_depth, -1,
-            ringbuffer.rb_header_id, ringbuffer.rb_hbuffer_id, ringbuffer.rb_dbuffer_id, ringbuffer.process_id,
-            -1, config.MPI_COMM_DELAY, c_det_def
-        )
+        udp_receive(udp_socket.fileno(), c_rb_meta, c_det_def, -1, config.MPI_COMM_DELAY)
 
         if control_client.is_message_ready():
             control_client.get_message()
