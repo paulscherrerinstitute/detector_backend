dec1e3ee962e77ad11ad4c2b8ce6b63cc1985f9a
Andrej Babic <andrej.babic@psi.ch>
2019-08-09 15:36:02 +0200

    Cleanup and refactor UDP receiver

--- src/udp_receiver.c
+++ src/udp_receiver.c
@@ -46,45 +46,21 @@ void save_packet (
   barebone_packet* bpacket, rb_metadata* rb_meta, counter* counters,
   detector* det, rb_header* header, rb_state* rb_current_state)
 {
-  
-  if (!is_slot_ready_for_frame(bpacket->framenum, counters))
-  {
-    commit_if_slot_dangling(counters, rb_meta, header, rb_current_state);
-    
-    claim_next_slot(rb_meta, rb_current_state);
-
-    initialize_rb_header(header, rb_meta, bpacket);
-
-    initialize_counters_for_new_frame(counters, bpacket->framenum);
-  }
-
   counters->current_frame_recv_packets++;
   counters->total_recv_packets++;
 
-  int line_number = get_packet_line_number(rb_meta, bpacket->packetnum);  
+  long dest_offset = bpacket->packetnum * det->bytes_data_per_packet;
 
-  copy_data(*det, *rb_meta, *rb_current_state, bpacket->data, line_number);
+  memcpy(
+      (char*) (rb_meta.data_slot_origin) + dest_offset,
+      (char*) bpacket->data,
+      det->bytes_data_per_packet
+  );
 
   update_rb_header(header, bpacket);
-
-  if(is_frame_complete(rb_meta->n_packets_per_frame, counters))
-  {
-    #ifdef DEBUG
-      printf("[save_packet][mod_number %d] Frame complete, got packet %d  #%d of %d frame %lu / %lu\n", 
-        rb_meta->mod_number, bpacket->packetnum, counters->current_frame_recv_packets, 
-        rb_meta->n_packets_per_frame, bpacket->framenum, counters->current_frame);
-    #endif
-
-    copy_rb_header(header, rb_current_state, counters, rb_meta->n_packets_per_frame);
-
-    commit_slot(rb_meta->rb_writer_id, rb_current_state->rb_current_slot);
-
-    counters->current_frame = NO_CURRENT_FRAME;
-    counters->total_recv_frames++;
-  }
 }
 
-int put_data_in_rb (int sock, rb_metadata rb_meta, detector det, uint32_t n_frames, float timeout)
+void put_data_in_rb (int sock, rb_metadata rb_meta, detector det, float timeout)
 {
 
   struct timeval udp_socket_timeout;
@@ -94,12 +70,6 @@ int put_data_in_rb (int sock, rb_metadata rb_meta, detector det, uint32_t n_fram
 
   struct timeval timeout_start_time;
   gettimeofday(&timeout_start_time, NULL);
-
-  uint64_t last_stats_print_total_recv_frames = 0;
-  
-  struct timeval last_stats_print_time;
-  gettimeofday(&last_stats_print_time, NULL);
-
 
   char udp_packet[det.bytes_per_packet];
   barebone_packet bpacket;
@@ -115,35 +85,45 @@ int put_data_in_rb (int sock, rb_metadata rb_meta, detector det, uint32_t n_fram
       sock, (char*)&udp_packet, det.bytes_per_packet, &bpacket
     );
 
-    if (is_packet_received) 
+    if (!is_packet_received)
     {
-      save_packet(&bpacket, &rb_meta, &counters, &det, &header, &rb_current_state);
-
-      // Reset timeout time.
-      gettimeofday(&timeout_start_time, NULL);
+      if (is_timeout_expired(timeout, timeout_start_time))
+      {
+        commit_if_slot_dangling(&counters, &rb_meta, &header, &rb_current_state);
+        return;
+      }
+      continue;
     }
-    else if (is_timeout_expired(timeout, timeout_start_time))
+
+    if (!is_rb_slot_ready_for_packet(bpacket->framenum, counters))
     {
-      // If images are lost in the last frame.
+      // If we lost packets at the end of the frame, it was not committed.
       commit_if_slot_dangling(&counters, &rb_meta, &header, &rb_current_state);
 
-      break;
+      claim_next_slot(&rb_meta, &rb_current_state);
+
+      initialize_rb_header(&header, &rb_meta, &bpacket);
+      initialize_counters_for_new_frame(&counters, bpacket->framenum);
     }
 
-    if (is_acquisition_completed(n_frames, &counters)) 
+    save_packet(&bpacket, &rb_meta, &counters, &det, &header, &rb_current_state);
+
+    if(is_frame_complete(rb_meta.n_packets_per_frame, &counters))
     {
-      printf("[put_data_in_rb][mod_number %d] Acquisition finished.", rb_meta.mod_number);
-      break;
+      #ifdef DEBUG
+        printf("[save_packet][mod_number %d] Frame complete, got packet %d  #%d of %d frame %lu / %lu\n",
+          rb_meta.mod_number, bpacket.packetnum, counters.current_frame_recv_packets,
+          rb_meta.n_packets_per_frame, bpacket.framenum, counters.current_frame);
+      #endif
+
+      copy_rb_header(header, rb_current_state, counters, rb_meta->n_packets_per_frame);
+
+      commit_slot(rb_meta->rb_writer_id, rb_current_state->rb_current_slot);
+
+      counters->current_frame = NO_CURRENT_FRAME;
+      counters->total_recv_frames++;
     }
 
-    if (counters.total_recv_frames % PRINT_STATS_N_FRAMES_MODULO == 0 
-      && counters.total_recv_frames != last_stats_print_total_recv_frames)
-    {
-      print_statistics(&counters, &last_stats_print_time);
-
-      last_stats_print_total_recv_frames = counters.total_recv_frames;
-    }
+    gettimeofday(&timeout_start_time, NULL);
   }
-
-  return counters.total_recv_frames + counters.total_lost_frames;
 }
