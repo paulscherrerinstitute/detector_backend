22e3797b957de9b8f2e8cc29a0e5f342d52bd72d
Andrej Babic <andrej.babic@psi.ch>
2019-08-13 14:53:57 +0200

    Improve udp_receiver launcher

--- detector_backend/module/udp_receiver.py
+++ detector_backend/module/udp_receiver.py
@@ -35,15 +35,6 @@ class CRbMetadata(ctypes.Structure):
 
         ('rb_hbuffer_id', ctypes.c_uint),
         ('rb_dbuffer_id', ctypes.c_uint),
-
-        ('mod_origin', ctypes.c_uint32),
-        ('mod_number', ctypes.c_uint),
-        ('n_lines_per_packet', ctypes.c_uint),
-        ('n_packets_per_frame', ctypes.c_uint),
-        ('bit_depth', ctypes.c_uint),
-
-        ('n_bytes_per_frame_line', ctypes.c_uint32),
-        ('n_bytes_per_submodule_line', ctypes.c_uint32),
     ]
 
 
@@ -66,16 +57,14 @@ def get_udp_receive_function():
 
 def get_c_det_submodule(detector_def, submodule_index):
     c_det_submodule = CDetectorSubmodule()
+
     c_det_submodule.submodule_index = submodule_index
     c_det_submodule.n_packets_per_frame = get_n_packets_per_frame(detector_def)
 
     detector_model = detector_def.detector_model
 
-    c_det_submodule.bytes_per_packet = as_ctypes(
-        array(detector_model.bytes_per_packet, dtype="uint32", order='C'))
-
-    c_det_submodule.bytes_data_per_packet = as_ctypes(
-        array(detector_model.bytes_data_per_packet, dtype="uint32", order='C'))
+    c_det_submodule.bytes_per_packet = detector_model.bytes_per_packet
+    c_det_submodule.bytes_data_per_packet = detector_model.bytes_data_per_packet
 
     c_det_submodule.submodule_data_slot_offset = \
         c_det_submodule.submodule_index * c_det_submodule.bytes_data_per_packet * c_det_submodule.n_packets_per_frame
@@ -97,12 +86,12 @@ def get_c_rb_metadata(ringbuffer):
 
 def start_udp_receiver(udp_ip, udp_port, detector_def, submodule_index, ringbuffer, control_client):
 
-    _logger.info("Starting udp_receiver submodule_index=%d with udp_ip='%s', udp_port=%d, " %
-                 (submodule_index, udp_ip, udp_port))
+    _logger.info("[%d] Starting udp_receiver submodule_index=%d with udp_ip='%s', udp_port=%d, " %
+                 (submodule_index, submodule_index, udp_ip, udp_port))
 
     ringbuffer.init_buffer()
 
-    _logger.debug("[%d] Ringbuffer initialized." % udp_port)
+    _logger.debug("[%d] Ringbuffer initialized." % submodule_index)
 
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, UDP_RCVBUF_SIZE)
@@ -117,7 +106,7 @@ def start_udp_receiver(udp_ip, udp_port, detector_def, submodule_index, ringbuff
 
     while True:
 
-        _logger.debug("[%d] Starting udp_receive function." % udp_port)
+        _logger.debug("[%d] Starting udp_receive function." % submodule_index)
 
         udp_receive(udp_socket.fileno(), c_rb_meta, c_det_submodule, config.MPI_COMM_DELAY)
 
