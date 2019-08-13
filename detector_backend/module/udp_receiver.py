dd96e9fa97ea6637256ef4617f428ad53bf4d199
Andrej Babic <andrej.babic@psi.ch>
2019-08-13 14:19:45 +0200

    Fix function signature bug

--- detector_backend/module/udp_receiver.py
+++ detector_backend/module/udp_receiver.py
@@ -9,9 +9,7 @@ import os
 import sys
 
 from detector_backend import config
-from detector_backend.utils_detectors import get_n_bytes_per_submodule_line, get_n_bytes_per_frame_line, \
-    get_n_packets_per_frame, get_n_lines_per_packet, \
-    get_current_module_index, get_current_module_offset_in_pixels
+from detector_backend.utils_detectors import get_n_packets_per_frame
 
 _logger = getLogger("udp_receiver")
 
@@ -20,13 +18,13 @@ UDP_RCVBUF_SIZE = 1 * 1024 * 1024
 
 class CDetectorSubmodule(ctypes.Structure):
     _fields_ = [
-        ('submodule_index', ctypes.uint16_t),
-        ('n_packets_per_frame', ctypes.uint16_t),
+        ('submodule_index', ctypes.c_uint16),
+        ('n_packets_per_frame', ctypes.c_uint16),
 
-        ('bytes_per_packet', ctypes.uint32_t),
-        ('bytes_data_per_packet', ctypes.uint32_t),
+        ('bytes_per_packet', ctypes.c_uint32),
+        ('bytes_data_per_packet', ctypes.c_uint32),
 
-        ('submodule_data_slot_offset', ctypes.uint64_t)
+        ('submodule_data_slot_offset', ctypes.c_uint64)
     ]
 
 
@@ -56,7 +54,7 @@ def get_udp_receive_function():
         _mod = ctypes.cdll.LoadLibrary(expected_library_location)
 
         put_data_in_rb = _mod.put_data_in_rb
-        put_data_in_rb.argtypes = (ctypes.c_int, CRbMetadata, CDetectorSubmodule, ctypes.c_uint32, ctypes.c_float)
+        put_data_in_rb.argtypes = (ctypes.c_int, CRbMetadata, CDetectorSubmodule, ctypes.c_float)
         put_data_in_rb.restype = ctypes.c_int
 
         return put_data_in_rb
@@ -66,9 +64,9 @@ def get_udp_receive_function():
         sys.exit(-1)
 
 
-def get_c_det_submodule(detector_def, submodule_id):
+def get_c_det_submodule(detector_def, submodule_index):
     c_det_submodule = CDetectorSubmodule()
-    c_det_submodule.submodule_index = submodule_id
+    c_det_submodule.submodule_index = submodule_index
     c_det_submodule.n_packets_per_frame = get_n_packets_per_frame(detector_def)
 
     detector_model = detector_def.detector_model
@@ -85,7 +83,7 @@ def get_c_det_submodule(detector_def, submodule_id):
     return c_det_submodule
 
 
-def get_c_rb_metadata(detector_def, ringbuffer, module_id, submodule_id):
+def get_c_rb_metadata(ringbuffer):
     c_rb_meta = CRbMetadata()
 
     c_rb_meta.rb_writer_id = ringbuffer.rb_consumer_id
@@ -94,23 +92,13 @@ def get_c_rb_metadata(detector_def, ringbuffer, module_id, submodule_id):
     c_rb_meta.rb_hbuffer_id = ringbuffer.rb_hbuffer_id
     c_rb_meta.rb_dbuffer_id = ringbuffer.rb_dbuffer_id
 
-    c_rb_meta.mod_origin = get_current_module_offset_in_pixels(detector_def, module_id, submodule_id)
-    c_rb_meta.mod_number = get_current_module_index(detector_def, module_id, submodule_id)
-
-    c_rb_meta.n_lines_per_packet = get_n_lines_per_packet(detector_def)
-    c_rb_meta.n_packets_per_frame = get_n_packets_per_frame(detector_def)
-    c_rb_meta.bit_depth = detector_def.bit_depth
-
-    c_rb_meta.n_bytes_per_frame_line = get_n_bytes_per_frame_line(detector_def)
-    c_rb_meta.n_bytes_per_submodule_line = get_n_bytes_per_submodule_line(detector_def)
-
     return c_rb_meta
 
 
-def start_udp_receiver(udp_ip, udp_port, detector_def, submodule_id, ringbuffer, control_client):
+def start_udp_receiver(udp_ip, udp_port, detector_def, submodule_index, ringbuffer, control_client):
 
-    _logger.info("Starting udp_receiver with udp_ip='%s', udp_port=%d, submodule_id=%d" %
-                 (udp_ip, udp_port, submodule_id))
+    _logger.info("Starting udp_receiver submodule_index=%d with udp_ip='%s', udp_port=%d, " %
+                 (submodule_index, udp_ip, udp_port))
 
     ringbuffer.init_buffer()
 
@@ -120,13 +108,11 @@ def start_udp_receiver(udp_ip, udp_port, detector_def, submodule_id, ringbuffer,
     udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, UDP_RCVBUF_SIZE)
     udp_socket.bind((udp_ip, udp_port))
 
-    c_det_submodule = get_c_det_submodule(detector_def, submodule_id)
-    c_rb_meta = get_c_rb_metadata(detector_def, ringbuffer, module_id, submodule_id)
+    c_det_submodule = get_c_det_submodule(detector_def, submodule_index)
+    c_rb_meta = get_c_rb_metadata(ringbuffer)
 
     # Function signature
-    # int sock, int bit_depth,
-    # int rb_current_slot, int rb_header_id, int rb_hbuffer_id, int rb_dbuffer_id, int rb_writer_id,
-    # uint32_t n_frames, float timeout, detector_definition det
+    # int sock, rb_metadata rb_meta, detector_submodule det_submodule, float timeout
     udp_receive = get_udp_receive_function()
 
     while True:
