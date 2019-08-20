96af5905780f9eb2fe0f089d44c36d64c526a34c
Andrej Babic <andrej.babic@psi.ch>
2019-08-20 15:21:22 +0200

    First attempt at an image assembler

--- detector_backend/module/rb_assembler.py
+++ detector_backend/module/rb_assembler.py
@@ -1,22 +1,44 @@
+import ctypes
+import os
+from _ctypes import POINTER
 from logging import getLogger
 import ringbuffer as rb
 
-import zmq
-
 from time import time, sleep
 
+from detector_backend import config
 from detector_backend.config import MPI_COMM_DELAY
+from detector_backend.detectors import DetectorDefinition
 from detector_backend.mpi_control import MpiControlClient
 from detector_backend.utils_ringbuffer import get_frame_metadata, get_frame_data
 
 _logger = getLogger("rb_assembler")
 
-RB_RETRY_DELAY = 0.01
-ZMQ_IO_THREADS = 4
+
+def get_image_assembler_function():
+    expected_library_location = os.path.dirname(os.path.realpath(__file__)) + "/../../libimageassembler.so"
+
+    try:
+        _mod = ctypes.cdll.LoadLibrary(expected_library_location)
+
+        assemble_image = _mod.assemble_image
+        assemble_image.argtypes = (ctypes.c_char_p, POINTER(ctypes.c_size_t), ctypes.c_uint32, ctypes.c_size_t)
+        assemble_image.restype = ctypes.c_int
+
+        return assemble_image
+
+    except:
+        _logger.error("Could not image assembler shared library from %s." % expected_library_location)
 
 
-def assemble_frame(metadata_pointer, data_pointer):
-    pass
+def read_frame(detector_def: DetectorDefinition, metadata_pointer, data_pointer):
+    data_shape = [detector_def.n_submodules_total] + detector_def.detector_model.submodule_size
+
+    data = get_frame_data(data_pointer, data_shape)
+    metadata = get_frame_metadata(metadata_pointer, detector_def.n_submodules_total)
+
+    return data, metadata
+
 
 def start_rb_assembler(name, detector_def, ringbuffer):
 
@@ -27,6 +49,10 @@ def start_rb_assembler(name, detector_def, ringbuffer):
     control_client = MpiControlClient()
 
     mpi_ref_time = time()
+
+    # Function signature
+    # char* source_root, char* destination_root, size_t* dest_move_offsets, uint32_t n_moves, size_t n_bytes_per_move
+    image_assembler = get_image_assembler_function()
 
     while True:
 
@@ -42,7 +68,7 @@ def start_rb_assembler(name, detector_def, ringbuffer):
 
         rb_current_slot = rb.claim_next_slot(ringbuffer.rb_reader_id)
         if rb_current_slot == -1:
-            sleep(RB_RETRY_DELAY)
+            sleep(config.RB_RETRY_DELAY)
             continue
 
         # TODO: Do the actual conversion.
