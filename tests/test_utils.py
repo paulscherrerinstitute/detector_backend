24346c8de15796045f233bfa17461c30dff983c0
Andrej Babic <andrej.babic@psi.ch>
2019-08-21 11:41:49 +0200

    Adjust tests to check for the assembled buffer as well

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,43 +16,55 @@ class UtilsTests(unittest.TestCase):
 
     def test_create_rb_files(self):
         self.assertFalse(os.path.isfile(config.DEFAULT_RB_IMAGE_HEAD_FILE))
-        self.assertFalse(os.path.isfile(config.DEFAULT_RB_IMAGE_DATA_FILE))
+        self.assertFalse(os.path.isfile(config.DEFAULT_RB_RAW_IMAGE_DATA_FILE))
+        self.assertFalse(os.path.isfile(config.DEFAULT_RB_ASSEMBLED_IMAGE_DATA_FILE))
 
         n_slots = 10
         n_head_slot_bytes = 64
-        n_data_slot_bytes = 4096
+        n_raw_data_slot_bytes = 4096
+        n_assembled_data_slot_bytes = 5000
 
-        create_rb_files(n_slots, n_head_slot_bytes, n_data_slot_bytes)
+        create_rb_files(n_slots, n_head_slot_bytes, n_raw_data_slot_bytes, n_assembled_data_slot_bytes)
 
         self.assertTrue(os.path.isfile(config.DEFAULT_RB_IMAGE_HEAD_FILE))
-        self.assertTrue(os.path.isfile(config.DEFAULT_RB_IMAGE_DATA_FILE))
+        self.assertTrue(os.path.isfile(config.DEFAULT_RB_RAW_IMAGE_DATA_FILE))
+        self.assertTrue(os.path.isfile(config.DEFAULT_RB_ASSEMBLED_IMAGE_DATA_FILE))
 
         head_file_size = os.path.getsize(config.DEFAULT_RB_IMAGE_HEAD_FILE)
         self.assertEqual(head_file_size, n_slots*n_head_slot_bytes)
 
-        data_file_size = os.path.getsize(config.DEFAULT_RB_IMAGE_DATA_FILE)
-        self.assertEqual(data_file_size, n_slots*n_data_slot_bytes)
+        raw_data_file_size = os.path.getsize(config.DEFAULT_RB_RAW_IMAGE_DATA_FILE)
+        self.assertEqual(raw_data_file_size, n_slots*n_raw_data_slot_bytes)
+
+        assembled_data_file_size = os.path.getsize(config.DEFAULT_RB_ASSEMBLED_IMAGE_DATA_FILE)
+        self.assertEqual(assembled_data_file_size, n_slots * n_assembled_data_slot_bytes)
 
     def test_exception(self):
         n_slots = -1
         n_head_slot_bytes = 64
-        n_data_slot_bytes = 4096
+        n_raw_data_slot_bytes = 4096
+        n_assembled_data_slot_bytes = 4096
 
         with self.assertRaisesRegex(RuntimeError, "Could not create file"):
-            create_rb_files(n_slots, n_head_slot_bytes, n_data_slot_bytes)
+            create_rb_files(n_slots, n_head_slot_bytes, n_raw_data_slot_bytes, n_assembled_data_slot_bytes)
 
     def test_overwrite(self):
         n_slots = 10
         n_head_slot_bytes = 64
-        n_data_slot_bytes = 4096
+        n_raw_data_slot_bytes = 4096
+        n_assembled_data_slot_bytes = 5000
 
-        create_rb_files(n_slots, n_head_slot_bytes, n_data_slot_bytes)
+        create_rb_files(n_slots, n_head_slot_bytes, n_raw_data_slot_bytes, n_assembled_data_slot_bytes)
 
         self.assertEqual(os.path.getsize(config.DEFAULT_RB_IMAGE_HEAD_FILE), n_slots * n_head_slot_bytes)
-        self.assertEqual(os.path.getsize(config.DEFAULT_RB_IMAGE_DATA_FILE), n_slots * n_data_slot_bytes)
+        self.assertEqual(os.path.getsize(config.DEFAULT_RB_RAW_IMAGE_DATA_FILE), n_slots * n_raw_data_slot_bytes)
+        self.assertEqual(os.path.getsize(config.DEFAULT_RB_ASSEMBLED_IMAGE_DATA_FILE),
+                         n_slots * n_assembled_data_slot_bytes)
 
         n_slots = 5
-        create_rb_files(n_slots, n_head_slot_bytes, n_data_slot_bytes)
+        create_rb_files(n_slots, n_head_slot_bytes, n_raw_data_slot_bytes, n_assembled_data_slot_bytes)
 
         self.assertEqual(os.path.getsize(config.DEFAULT_RB_IMAGE_HEAD_FILE), n_slots * n_head_slot_bytes)
-        self.assertEqual(os.path.getsize(config.DEFAULT_RB_IMAGE_DATA_FILE), n_slots * n_data_slot_bytes)
+        self.assertEqual(os.path.getsize(config.DEFAULT_RB_RAW_IMAGE_DATA_FILE), n_slots * n_raw_data_slot_bytes)
+        self.assertEqual(os.path.getsize(config.DEFAULT_RB_ASSEMBLED_IMAGE_DATA_FILE),
+                         n_slots * n_assembled_data_slot_bytes)
