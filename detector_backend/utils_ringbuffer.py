f58f4220066b0c1e8df7f6fe2c157a88a0fdaab8
Andrej Babic <andrej.babic@psi.ch>
2019-08-21 11:31:36 +0200

    Create also assembled data buffer

--- detector_backend/utils_ringbuffer.py
+++ detector_backend/utils_ringbuffer.py
@@ -11,9 +11,11 @@ _logger = logging.getLogger("utils_ringbuffer")
 
 def create_rb_files(n_slots,
                     n_header_slot_bytes,
-                    n_data_slot_bytes,
+                    n_raw_data_slot_bytes,
+                    n_assembled_data_slot_bytes,
                     image_head_file=config.DEFAULT_RB_IMAGE_HEAD_FILE,
-                    image_data_file=config.DEFAULT_RB_IMAGE_DATA_FILE):
+                    raw_image_data_file=config.DEFAULT_RB_RAW_IMAGE_DATA_FILE,
+                    assembled_image_data_file=config.DEFAULT_RB_ASSEMBLED_IMAGE_DATA_FILE):
 
     def create_file(output_file, block_size, n_blocks):
         _logger.debug("Creating file %s with block_size %d and n_blocks %n",
@@ -29,9 +31,11 @@ def create_rb_files(n_slots,
                                (output_file, block_size, n_blocks))
 
     create_file(image_head_file, n_header_slot_bytes, n_slots)
-    create_file(image_data_file, n_data_slot_bytes, n_slots)
+    create_file(raw_image_data_file, n_raw_data_slot_bytes, n_slots)
+    create_file(assembled_image_data_file, n_assembled_data_slot_bytes, n_slots)
 
-    _logger.info("Ringbuffer files %s and %s created.", image_head_file, image_data_file)
+    _logger.info("Ringbuffer files %s, %s and %s created.",
+                 image_head_file, raw_image_data_file, assembled_image_data_file)
 
 
 class CRingBufferImageHeaderData(ctypes.Structure):
