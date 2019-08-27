c498be71af310e11642a838de973ebee1bc98f34
Andrej Babic <andrej.babic@psi.ch>
2019-08-27 17:53:30 +0200

    Add submodule n_bytes

--- detector_backend/detectors.py
+++ detector_backend/detectors.py
@@ -27,6 +27,30 @@ class DetectorModel(object):
         self.gap_px_modules = gap_px_modules
 
 
+EIGER = DetectorModel(
+    model_name="Eiger PSI",
+    module_size=[512, 1024],
+    submodule_size=[256, 512],
+    n_submodules_per_module=4,
+    bytes_per_packet=4144,
+    bytes_data_per_packet=4096,
+    gap_px_chips=[2, 2],
+    gap_px_modules=[36, 8]
+)
+
+
+JUNGFRAU = DetectorModel(
+    model_name="Jungfrau",
+    module_size=[512, 1024],
+    submodule_size=[512, 1024],
+    n_submodules_per_module=1,
+    bytes_per_packet=8246,
+    bytes_data_per_packet=8192,
+    gap_px_chips=[0, 0],
+    gap_px_modules=[0, 0]
+)
+
+
 class DetectorDefinition(object):
 
     def __init__(self, detector_name, detector_model: DetectorModel, geometry, bit_depth, ignored_modules=None):
@@ -57,30 +81,8 @@ class DetectorDefinition(object):
         self.image_header_n_bytes = config.IMAGE_HEADER_SUBMODULE_SIZE_BYTES * self.n_submodules_total
         self.raw_image_data_n_bytes = (self.detector_size_raw[0] * self.detector_size_raw[1] * self.bit_depth) // 8
         self.image_data_n_bytes = (self.detector_size[0] * self.detector_size[1] * self.bit_depth) // 8
+        self.submodule_data_n_bytes = (self.detector_model.submodule_size[0] *
+                                       self.detector_model.submodule_size[1] * self.bit_depth) // 8
 
         self.submodule_line_n_bytes = (self.detector_model.submodule_size[1] * self.bit_depth) // 8
         self.image_line_n_bytes = (self.detector_size[1] * self.bit_depth) // 8
-
-
-EIGER = DetectorModel(
-    model_name="Eiger PSI",
-    module_size=[512, 1024],
-    submodule_size=[256, 512],
-    n_submodules_per_module=4,
-    bytes_per_packet=4144,
-    bytes_data_per_packet=4096,
-    gap_px_chips=[2, 2],
-    gap_px_modules=[36, 8]
-)
-
-
-JUNGFRAU = DetectorModel(
-    model_name="Jungfrau",
-    module_size=[512, 1024],
-    submodule_size=[512, 1024],
-    n_submodules_per_module=1,
-    bytes_per_packet=8246,
-    bytes_data_per_packet=8192,
-    gap_px_chips=[0, 0],
-    gap_px_modules=[0, 0]
-)
