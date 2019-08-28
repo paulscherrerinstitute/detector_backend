ec3b8954cd9694553dd82aae16fd56706cabfcc3
Andrej Babic <andrej.babic@psi.ch>
2019-08-28 15:04:00 +0200

    Add test to check the number of moves

--- tests/test_rb_assembler.py
+++ tests/test_rb_assembler.py
@@ -1,4 +1,6 @@
 import unittest
+
+from detector_backend.assemblers import JungfrauAssembler
 from detector_backend.detectors import DetectorDefinition, JUNGFRAU
 from detector_backend.module.rb_assembler import ImageAssembler
 from detector_backend.utils_ringbuffer import create_rb_files
@@ -25,13 +27,24 @@ class RbAssemblerTests(unittest.TestCase):
         )
 
         with self.assertRaisesRegex(ValueError, "Wrong number of assemblers"):
-            ImageAssembler(jf_test_det, 0, 7)
+            ImageAssembler(JungfrauAssembler(jf_test_det), 0, 7)
 
-        n_assemblers = 4
-        image_assember = ImageAssembler(jf_test_det, 0, n_assemblers)
-        self.assertEqual(image_assember.n_moves * n_assemblers,
-                         jf_test_det.raw_image_data_n_bytes / jf_test_det.submodule_line_n_bytes,
-                         "All assemblers combined should combine the entire image.")
+        total_moves = jf_test_det.raw_image_data_n_bytes // jf_test_det.submodule_line_n_bytes
+
+        for n_assemblers in range(1, 12):
+            with self.subTest(n_assemblers=n_assemblers):
+                if total_moves % n_assemblers == 0:
+
+                    image_assember = ImageAssembler(JungfrauAssembler(jf_test_det), 0, n_assemblers)
+                    self.assertEqual(image_assember.n_moves * n_assemblers, total_moves,
+                                     "All assemblers combined should combine the entire image.")
+
+                    self.assertEqual(len(image_assember.move_offsets), image_assember.n_moves)
+                    self.assertEqual(len(image_assember.move_offsets) * n_assemblers, total_moves)
+
+                else:
+                    with self.assertRaisesRegex(ValueError, "Wrong number of assemblers"):
+                        ImageAssembler(JungfrauAssembler(jf_test_det), 0, n_assemblers)
 
     def test_ImageAssembler_move_offsets(self):
         # TODO: Implement this maybe.
