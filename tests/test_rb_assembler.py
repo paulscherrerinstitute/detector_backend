466007d08665ef1828f58a042fffe492ecbfa9ec
Andrej Babic <andrej.babic@psi.ch>
2019-09-03 11:07:59 +0200

    Add tests for the RB_assembly

--- tests/test_rb_assembler.py
+++ tests/test_rb_assembler.py
@@ -1,8 +1,12 @@
+import ctypes
 import unittest
+from _ctypes import POINTER
+
+import numpy
 
 from detector_backend.assemblers import JungfrauAssembler
 from detector_backend.detectors import DetectorDefinition, JUNGFRAU
-from detector_backend.module.rb_assembler import ImageAssembler
+from detector_backend.module.rb_assembler import ImageAssembler, get_image_assembler_function
 from detector_backend.utils_ringbuffer import create_rb_files
 from tests.utils import cleanup_rb_files
 
@@ -28,6 +32,9 @@ class RbAssemblerTests(unittest.TestCase):
 
         with self.assertRaisesRegex(ValueError, "Wrong number of assemblers"):
             ImageAssembler(JungfrauAssembler(jf_test_det), 0, 7)
+
+        with self.assertRaisesRegex(ValueError, "must be smaller"):
+            ImageAssembler(JungfrauAssembler(jf_test_det), 1, 1)
 
         total_moves = jf_test_det.raw_image_data_n_bytes // jf_test_det.submodule_line_n_bytes
 
@@ -74,6 +81,42 @@ class RbAssemblerTests(unittest.TestCase):
                     total_moves += image_assember.move_offsets
 
                 self.assertListEqual(total_moves, jf_assembler.get_move_offsets())
+
+    def test_ImageAssembler_JF_image(self):
+        det_def = DetectorDefinition(
+            detector_name="",
+            detector_model=JUNGFRAU,
+            geometry=[4, 3],
+            bit_depth=16
+        )
+
+        image_assembler = ImageAssembler(JungfrauAssembler(detector_def=det_def), 0, 1)
+
+        raw_image = numpy.zeros(shape=[det_def.n_submodules_total] + det_def.detector_model.submodule_size,
+                                dtype="uint%d" % det_def.bit_depth)
+        final_image = numpy.zeros(shape=det_def.detector_size,
+                                  dtype="uint%d" % det_def.bit_depth)
+        move_offsets = numpy.array(image_assembler.move_offsets, dtype="uint32")
+
+        n_packets_per_image = det_def.submodule_data_n_bytes // det_def.detector_model.bytes_data_per_packet
+        n_lines_per_packet = det_def.detector_model.bytes_data_per_packet // det_def.submodule_line_n_bytes
+
+        for i_submodule in range(det_def.n_submodules_total):
+            for i_packet in range(n_packets_per_image):
+                for i_line in range(n_lines_per_packet):
+                    i_absolute_line = (i_packet * n_lines_per_packet) + i_line
+                    fill_value = i_submodule * 1000 + i_absolute_line
+
+                    raw_image[i_submodule][i_absolute_line].fill(fill_value)
+
+        assemble_image = get_image_assembler_function()
+
+        # TODO: Segmentation fault. Check the C pointers from the numpy array first.
+        assemble_image(raw_image.ctypes.data_as(ctypes.c_char_p),
+                       final_image.ctypes.data_as(ctypes.c_char_p),
+                       move_offsets.ctypes.data_as(POINTER(ctypes.c_size_t)),
+                       image_assembler.n_moves,
+                       10)
 
 
 if __name__ == "__main__":
