d58b9ed75c0dfc12fb84767def8735588406a02f
Andrej Babic <andrej.babic@psi.ch>
2019-09-03 15:10:48 +0200

    We specify the RB folder only
    Files all have standard names anyway.

--- detector_backend/config.py
+++ detector_backend/config.py
@@ -1,8 +1,9 @@
-DEFAULT_RB_FOLDER = "/dev/shm/rb/"
-DEFAULT_RB_HEAD_FILE = "rb_header.dat"
-DEFAULT_RB_IMAGE_HEAD_FILE = "rb_image_header.dat"
-DEFAULT_RB_RAW_IMAGE_DATA_FILE = "rb_raw_image_data.dat"
-DEFAULT_RB_ASSEMBLED_IMAGE_DATA_FILE = "rb_assembled_image_data.dat"
+# Ring buffer files and folders.
+DEFAULT_RB_FOLDER = ""
+RB_HEAD_FILE = "rb_header.dat"
+RB_IMAGE_HEAD_FILE = "rb_image_header.dat"
+RB_RAW_IMAGE_DATA_FILE = "rb_raw_image_data.dat"
+RB_ASSEMBLED_IMAGE_DATA_FILE = "rb_assembled_image_data.dat"
 
 # 64 bytes per submodule = 8 * uint64_t values == 8 * (64bit/8bit)
 IMAGE_HEADER_SUBMODULE_SIZE_BYTES = 64
