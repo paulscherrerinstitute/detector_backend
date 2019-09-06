2f09ec82aca7f8f80c1cfb478107e0b8a2a4fe74
Andrej Babic <andrej.babic@psi.ch>
2019-09-06 09:28:25 +0200

    Add metatada writing to writer

--- detector_backend/module/h5_writer.py
+++ detector_backend/module/h5_writer.py
@@ -5,14 +5,33 @@ import numpy
 
 from detector_backend.detectors import DetectorDefinition
 
-IMAGE_DATASET_NAME = "/data/"
-INITIAL_IMAGE_DATASET_SIZE = 1000
-
 _logger = logging.getLogger("h5_writer")
 
 
+IMAGE_DATASET_NAME = "/data/"
+INITIAL_IMAGE_DATASET_SIZE = 1000
+METADATA_DATASET_NAME_FORMAT = "/data/%s/%s"
+
+# metadata_name: (metdata_type, per_module)
+METADATA_MAPPING = {
+    "pulse_id": ("uint64", False),
+    "frame": ("uint64", False),
+    "is_good_frame": ("uint64", False),
+    "daq_rec": ("int64", False),
+
+    "missing_packets_1": ("uint64", True),
+    "missing_packets_2": ("uint64", True),
+    "daq_recs": ("daq_recs", True),
+
+    "pulse_ids": ("uint64", True),
+    "framenums": ("uint64", True),
+}
+
+
 class H5Writer(object):
-    def __init__(self, detector_def: DetectorDefinition, output_file="/dev/null", parameters=None):
+    def __init__(self, detector_def: DetectorDefinition,
+                 output_file="/dev/null",
+                 parameters=None):
         self.detector_def = detector_def
         self.output_file = output_file
         self.parameters = parameters or {}
@@ -51,26 +70,30 @@ class H5Writer(object):
         self.file.create_dataset("/general/detector_name",
                                  data=numpy.string_(self.detector_def.detector_name))
 
-
-
     def write_image(self, image_bytes):
         self.image_dataset.id.write_direct_chunk((self.image_write_index, 0, 0), image_bytes)
         self.image_write_index += 1
 
-    def write_medadata(self, metadata):
+    def write_metadata(self, metadata):
         for name, value in metadata.items():
 
             if name not in self.cache:
                 self.cache[name] = []
 
-            self.cache.append(value)
+            self.cache[name].append(value)
 
     def _flush_metadata(self):
         if not self.cache:
-            return
+            _logger.info("No metadata in cache. Returning")
 
         for name, value in self.cache.items():
-            pass
+            dataset_name = METADATA_DATASET_NAME_FORMAT % (self.detector_def.detector_name, name)
+            dtype = METADATA_MAPPING[name][0]
+
+            _logger.debug("Creating dataset_name=%s and dtype=%s", dataset_name, dtype)
+
+            dataset = numpy.array(value, dtype=dtype)
+            self.file[dataset_name] = dataset
 
     def close(self):
         self._flush_metadata()
