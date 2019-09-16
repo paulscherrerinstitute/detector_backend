8f2ad87e8dfe5a77807754b737aa439d38044dc6
Andrej Babic <andrej.babic@psi.ch>
2019-09-16 15:45:25 +0200

    Fix space indentation

--- tests/manual/manual_test_jf4m_alvra.py
+++ tests/manual/manual_test_jf4m_alvra.py
@@ -3,12 +3,11 @@ import logging
 from detector_backend.profile.receive_write import receive_and_write
 from detector_backend.detectors import DetectorDefinition, JUNGFRAU
 
-
 default_logging_level = logging.INFO
 logging.basicConfig(level=default_logging_level)
 
 # logging.getLogger("rest_api").setLevel(logging.DEBUG)
-#logging.getLogger("udp_receiver").setLevel(logging.DEBUG)
+# logging.getLogger("udp_receiver").setLevel(logging.DEBUG)
 # logging.getLogger("rb_assembler").setLevel(logging.DEBUG)
 # logging.getLogger("zmq_sender").setLevel(logging.DEBUG)
 # logging.getLogger("mpi_ringbuffer_master").setLevel(logging.DEBUG)
@@ -24,13 +23,13 @@ jf4m = DetectorDefinition(
 )
 
 udp_ip_and_port = [
-	("10.30.20.6", 50060),
-	("10.30.20.6", 50061),
-	("10.30.20.6", 50062),
-	("10.30.20.6", 50063),
-	("10.30.20.6", 50064),
-	("10.30.20.6", 50065),
-	("10.30.20.6", 50066),
-	("10.30.20.6", 50067)]
+    ("10.30.20.6", 50060),
+    ("10.30.20.6", 50061),
+    ("10.30.20.6", 50062),
+    ("10.30.20.6", 50063),
+    ("10.30.20.6", 50064),
+    ("10.30.20.6", 50065),
+    ("10.30.20.6", 50066),
+    ("10.30.20.6", 50067)]
 
 receive_and_write(jf4m, udp_ip_and_port)
